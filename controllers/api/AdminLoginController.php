9e6e4660847de7d579ac94f3e1f9038a344ad252
kopa <1113288996@qq.com>
2018-08-08 10:51:29 +0800

    初始化版本

---
+++ controllers/api/AdminLoginController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace app\controllers\api;
+
+use Yii;
+use yii\filters\AccessControl;
+use yii\web\Controller;
+use yii\web\Response;
+use yii\filters\VerbFilter;
+use app\models\LoginForm;
+use app\models\ContactForm;
+
+class AdminLoginController extends Controller
+{
+    function actionEcho(){
+        echo 'halo';
+    }
+}
